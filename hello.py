0ce227ec0b82b661e843de4a521a47bea65d50b7
Gustavo <gusjoseloz0803@gmail.com>
2024-08-26 09:20:11 -0500

    Hola mundo con Python

---
+++ hello.py
@@ -0,0 +1,16 @@
+#Esto es un comentario
+#Nuestro hola mundo en python
+print("Hola Python")
+print('Esto también es una cadena de texto')
+
+"""
+Este es un comenatario
+en varias lineas
+Let's go
+"""
+
+#Consultar el tipo de dato
+print(type("Mi nombre es: Gustavo"))
+print(type(5))
+print(type(5.6))
+print(type(5.6 + 1j))
